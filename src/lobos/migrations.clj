dbaae9f90176308960f0a35208f971cfca8f4d8c
Edward Tsech <edtsech@gmail.com>
2013-03-16 13:45:18 +0100

    Convert migration.

--- src/lobos/migrations.clj
+++ src/lobos/migrations.clj
@@ -2,3 +2,17 @@
   (:refer-clojure :exclude
                   [alter drop bigint boolean char double float time])
   (:use (lobos [migration :only [defmigration]] core schema config)))
+
+(defmigration add-users-table
+  (up [] (create
+          (table :users
+                 (varchar :id 20 :primary-key)
+                 (varchar :first_name 30)
+                 (varchar :last_name 30)
+                 (varchar :email 30)
+                 (boolean :admin)
+                 (time    :last_login)
+                 (boolean :is_active)
+                 (varchar :pass 100))))
+
+  (down [] (drop (table :users))))
--- src/ltest/models/schema.clj
+++ src/ltest/models/schema.clj
@@ -16,20 +16,6 @@
   []
   (.exists (new java.io.File (str (io/resource-path) db-store ".h2.db"))))
 
-(defn create-users-table
-  []
-  (sql/with-connection db-spec
-    (sql/create-table
-      :users
-      [:id "varchar(20) PRIMARY KEY"]
-      [:first_name "varchar(30)"]
-      [:last_name "varchar(30)"]
-      [:email "varchar(30)"]
-      [:admin :boolean]
-      [:last_login :time]
-      [:is_active :boolean]
-      [:pass "varchar(100)"])))
-
 (defn create-tables
   "creates the database tables used by the application"
   []
